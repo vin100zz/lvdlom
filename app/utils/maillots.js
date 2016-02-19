3a69ada5cc7aefe703af9bd0ebc697aea078e891
vin100zz <vin100france@yahoo.fr>
2016-02-19 19:08:09 +0100

    Added maillots

--- app/utils/maillots.js
+++ app/utils/maillots.js
@@ -55,7 +55,9 @@ app.service('Maillots', function() {
     274: {template: 1, color1: "6BBEE9", color2: "FFFFFF", color3: "ffffff"}, // NAPLES
     279: {template: 33, color1: "FFFFFF", color2: "008A57", color3: "008A57"}, // GRONINGUE
     280: {template: 1, color1: "0075D4", color2: "FFFFFF", color3: "ffffff"}, // LIBEREC,
-    281: {template: 2, color1: "E80300", color2: "FFFFFF", color3: "FFFFFF"},
+    281: {template: 2, color1: "E80300", color2: "FFFFFF", color3: "FFFFFF"}, // BRAGA
+    282: {template: 3, color1: "E31818", color2: "FFFFFF", color3: "1A171B"}, // BILBAO ATHLETIC
+    283: {template: 1, color1: "ffffff", color2: "ffffff", color3: "00AFE3"}, // GRANVILLE
     "ALÈS": {template: 36, color1: "034EA2", color2: "FFFFFF", color3: "FFFFFF"},
     "AJACCIO": {template: 48, color1: "FFFFFF", color2: "ED1C24", color3: "ED1C24"},
     "AJAX AMSTERDAM": {template: 55, color1: "FFFFFF", color2: "EC1346", color3: "EC1346"},
