14781c6c1c653523c12e04a835d4510aad6aef49
vin100zz <vincent.carle06@gmail.com>
2020-01-07 07:56:05 +0100

    4 remplacements

--- app/directives/saisie/common/match.js
+++ app/directives/saisie/common/match.js
@@ -49,7 +49,7 @@ app.directive('lvdlomSaisieCommonMatch', function () {
 
         // remplacants
         $scope.cfg.data.remplacants = [];
-        for (i=0; i<3; ++i) {
+        for (i=0; i<4; ++i) {
           $scope.cfg.data.remplacants.push({
             joueur: null,
             carton: '',
