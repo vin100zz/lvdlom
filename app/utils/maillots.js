1f05925ed71fc0c10c017517c387b6e344c99629
LAPTOP-SMMFSP13\carle <carle.rene@wanadoo.fr>
2023-08-19 19:41:16 +0200

    Maillots

--- app/utils/maillots.js
+++ app/utils/maillots.js
@@ -80,6 +80,10 @@ app.service('Maillots', function() {
     296: {template: 1, color1: "F40506", color2: "FBFF00", color3: "FBFF00"}, // CHAUVIGNY
     297: {template: 1, color1: "000000", color2: "ffffff", color3: "ffffff"}, // QARABAG
     298: {template: 5, color1: "FF321E", color2: "0051A3", color3: "ffffff"}, // BÂLE
+    299: {template: 3, color1: "FFFFFF", color2: "000000", color3: "000000"}, // SALONIQUE PAOK
+    300: {template: 1, color1: "FFFFFF", color2: "FFFFFF", color3: "002156"}, // TOTTENHAM
+    301: {template: 4, color1: "008863", color2: "FFFFFF", color3: "FFFFFF"}, // SPORTING PORTUGAL
+    302: {template: 1, color1: "00793F", color2: "FFFFFF", color3: "FFFFFF"}, // PANATHINAIKOS
     "ALÈS": {template: 36, color1: "034EA2", color2: "FFFFFF", color3: "FFFFFF"},
     "AJACCIO": {template: 48, color1: "FFFFFF", color2: "ED1C24", color3: "ED1C24"},
     "AJAX AMSTERDAM": {template: 55, color1: "FFFFFF", color2: "EC1346", color3: "EC1346"},
